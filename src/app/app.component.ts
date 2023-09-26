df78e88bd93758c7a39b0a0cb58c8bceb0805bac
manas-153 <sharmamanas153@gmail.com>
2023-09-26 14:41:39 +0530

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Swaraj_Login_Page_with_encryption';
+}
